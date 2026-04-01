e6fcb46cb6d96e67835dd1a24bc74d0eb53ee231
Ritvars <ritvarsbaders@gmail.com>
2026-04-01 11:15:01 +0300

    Added: controler for data and html file

--- src/main/java/lv/venta/controller/SimpleController.java
+++ src/main/java/lv/venta/controller/SimpleController.java
@@ -1,6 +1,9 @@
 package lv.venta.controller;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,6 +16,16 @@ public class SimpleController {
 	public String getShowPage() {
 		System.out.println("Mans primais kontrolieris");
 		return "show-page";//tiks paradita show-page.html lapa ieks web parluka
+	}
+	
+	@GetMapping("/data")//localhost:8080/simple/data
+	public String getDataInPage(Model model) {
+		System.out.println("Izpildas datu kontrolieris");
+		Random rand = new Random();
+		String data = "@Janis " + rand.nextInt(2000, 2026);
+		model.addAttribute("package", data);
+		return "show-data-page";//tiks paradita show-tdata-page.html lapa
+		
 	}
 	
 }
